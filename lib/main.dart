294b21dd1bd1dac61ddb0a165a72edf6a01f7446
ahmadassi99 <ahmad.assi.2133@gmail.com>
2021-11-08 17:29:39 +0200

    ahmad-edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onboarding_screen/information.dart';
 import 'package:onboarding_screen/scroll.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class BoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BoardingPage(),
+      home: Test(),
     );
   }
 }
